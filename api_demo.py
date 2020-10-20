70503e00279dbf89982b6aba4a9a86e9518f7716
deepaksai7 <deepaksai395@gmail.com>
2020-10-20 21:37:45 +0530

    api

---
+++ api_demo.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+SUBSCRIPTION_KEY="e202927a1f9b46d88ee79ed225f89fe4"
+vision_service_address="https://imageanalyzerpython.cognitiveservices.azure.com/"
+address=vision_service_address+"vision/v2.0/analyze"
+parameters={'visualFeatures':'Description,Color',
+'language':'en'}
+image_path="C:\myspace\codes\python_tutorial\images\polarbear.jpg"
+image_data=open(image_path,"rb").read()
+headers={'Content-Type':'application/octet-stream',
+'Ocp-Apim-Subscription-Key':SUBSCRIPTION_KEY}
+
+response=requests.post(address,headers=headers,params=parameters,data=image_data)
+response.raise_for_status()
+
+results=response.json()
+print(json.dumps(results))
